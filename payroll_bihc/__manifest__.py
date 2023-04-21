b0a0589fc63581d88202f1f9aa01084d240e4cf3
Stefan J <stefanj@avalantec.com>
2023-04-21 20:02:44 +0000

    Updated

--- payroll_bihc/__manifest__.py
+++ payroll_bihc/__manifest__.py
@@ -27,7 +27,6 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/project_task.xml'
     ],
     # only loaded in demonstration mode
     #'demo': [
